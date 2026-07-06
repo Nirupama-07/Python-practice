cb78092bee88ceb50a344c2164ae872305a38fc1
Nirupama <nirupamamajhi549@gmail.com>
2026-07-06 08:45:19 +0530

    Added while loop practice program

---
+++ loops/while.py
@@ -0,0 +1,30 @@
+#Write a program to print numbers from 1 to 10 using while.
+'''num=1
+
+while(num<=10):
+    print(num)
+    num+=1'''
+
+#Write a program to print even numbers from 1 to 20 using while.
+'''num=1
+
+while(num<=20):
+    if(num%2==0):
+        print(num)
+    num+=1'''
+
+#Write a program to find the sum of first 10 numbers using while.
+'''num=1
+sum=0
+
+while(num<=10):
+    sum=sum+num
+    num+=1
+print(sum)'''
+    
+#Write a program to reverse a number using while
+num=10
+print("Reverse is:")
+while(num>=1):
+    print(num)
+    num-=1
