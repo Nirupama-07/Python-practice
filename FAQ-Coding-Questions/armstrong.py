f3f4feeabc837a97489398f63056d59a7a2b7edd
Nirupama <nirupamamajhi549@gmail.com>
2026-07-07 06:22:52 +0530

    Added another problem

---
+++ FAQ-Coding-Questions/armstrong.py
@@ -0,0 +1,16 @@
+num=int(input("Enter a number"))
+
+temp=num
+total=0
+
+while num>0:
+    digit=num%10
+    total+=digit**3
+    num//=10
+
+if total==temp:
+    print("Armstrong")
+else:
+    print("Not armstrong")
+
+#armstrong: 153=1^3 + 5^3 + 3^3=153
