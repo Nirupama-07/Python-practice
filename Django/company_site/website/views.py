e256e1f826fdd0d714e857bab68bfb831be12aeb
Nirupama <nirupamamajhi549@gmail.com>
2026-07-19 07:14:14 +0530

    Modified some features

--- Django/company_site/website/views.py
+++ Django/company_site/website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    return render(request,"home.html",{"name":"Crushaders"})
 
 def about(request):
-    return render(request,"about.html")
+    return render(request,"about.html",{"name":"Crushaders"})
 
 def contact(request):
-    return render(request,"contact.html")
+    return render(request,"contact.html",{"name":"Crushaders"})
